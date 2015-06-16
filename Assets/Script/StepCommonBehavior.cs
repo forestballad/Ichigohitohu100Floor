12c3fb59057ce713199967942c1bbbfbc8b3f076
Anli Ji <anli.azuresand@gmail.com>
2015-06-16 18:48:59 -0400

    details, details.

--- Assets/Script/StepCommonBehavior.cs
+++ Assets/Script/StepCommonBehavior.cs
@@ -6,12 +6,12 @@ public class StepCommonBehavior : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		transform.position = new Vector2(-2.78f + 0.376f * Random.Range (0,11), -2.84f);
+		transform.position = new Vector2(-2.78f + 0.376f * Random.Range (0,11), -2.66f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (transform.position.y > 3f) {
+		if (transform.position.y > 2.6f) {
 			GameObject.Find("Scripts").GetComponent<GameController>().AddOneBlock();
 			Destroy(gameObject);
 		}
